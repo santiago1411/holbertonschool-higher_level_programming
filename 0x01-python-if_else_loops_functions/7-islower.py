6810864de91de37ba60c964433ba8de49c7c7eb0
santiago1411 <4547@holbertonschool.com>
2022-05-03 13:58:49 -0500

    mayusculas o minusculas

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if (ord(c) != range(97, 123)):
-        return False
-    else:
+    if (ord(c) == range(97, 123)):
         return True
+    else:
+        return False
