7647def83c217abb644102ab59803d6b9730165a
santiago1411 <4547@holbertonschool.com>
2022-05-05 10:57:19 -0500

    math operation

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__"
+from calculator_1 import sub, div, mul, add
+a = 10
+b = 5
+print(f"{a} + {b} = {add(a, b)}")
+print(f"{a} * {b} = {mul(a, b)}")
+print(f"{a} - {b} = {sub(a, b)}")
+print(f"{a} / {b} = {div(a, b)}")
