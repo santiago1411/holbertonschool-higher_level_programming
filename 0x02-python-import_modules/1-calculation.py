adc55fa5ff5f52db9a943d5558e183f0bd146b64
santiago1411 <4547@holbertonschool.com>
2022-05-05 16:19:57 -0500

    imports the function def add(a, b): from the file add_0.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     a = 10
     b = 5
     print(f"{a} + {b} = {add(a,b)}")
-    print(f"{a} * {b} = {mul(a,b)}")
     print(f"{a} - {b} = {sub(a,b)}")
+    print(f"{a} * {b} = {mul(a,b)}")
     print(f"{a} / {b} = {div(a,b)}")
