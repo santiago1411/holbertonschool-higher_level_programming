a33ca3e766c618a094ea22e3bc3a63380fc778b1
santiago1411 <4547@holbertonschool.com>
2022-05-09 12:13:41 -0500

    Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list_og = my_list.copy()
+    if idx < 0 or idx > len(my_list):
+        return list_og
+    list_og[idx] = element
+    return list_og
