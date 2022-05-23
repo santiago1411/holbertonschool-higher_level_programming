8db97e58819794639c4c44d5f2d39e41428bc0b0
santiago1411 <4547@holbertonschool.com>
2022-05-23 12:23:04 -0500

    Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         count = 0
         while count < x:
             print(my_list[count], end="")
-            count +=1
+            count += 1
         print()
     except IndexError:
         print()
