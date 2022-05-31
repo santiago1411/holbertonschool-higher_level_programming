60ba463e0747632b668771e09c51ec169e97cffe
santiago1411 <4547@holbertonschool.com>
2022-05-31 09:40:38 -0500

    Test-driven development

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,32 +1,83 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""write unittests
 """
+
 import unittest
+
 max_integer = __import__('6-max_integer').max_integer
 
-class TestMaxInteger(unittest.TestCase):
-    """ tests for max_integer
+
+class testing_max_integer(unittest.TestCase):
+    """Class test
+    Arguments:
+        unittest {[type]} -- [description]
     """
-
-    def test_max_int_basic(self):
-        """ tests normal list of ints
+    def test_middle(self):
+        """test_middle
         """
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEquals(max_integer([1, 2, 5, 4]), 5)
 
-    def test_max_int_empty(self):
-        """ tests if list is empty
+    def test_end(self):
+        """test_middle
         """
-        self.assertEqual(max_integer([]), None)
+        self.assertAlmostEquals(max_integer([1, 2, 4, 5]), 5)
 
-    def test_max_int_neg(self):
-        """ tests if list has a negative int
+    def test_beginning(self):
+        """test_middle
         """
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertAlmostEquals(max_integer([5, 2, 4, 2]), 5)
 
-    def test_max_int_one(self):
-        """ tests if list has only one item
+    def test_only_negative_numbers(self):
+        """test_middle
         """
-        self.assertEqual(max_integer([1]), 1)
+        self.assertAlmostEquals(max_integer([-1, -3, -100, -4]), -1)
+
+    def test_floats(self):
+        """test_middle
+        """
+        self.assertAlmostEqual(max_integer([1.1, 2.2, 3.3, 4.4]), 4.4)
+
+    def test_one_negative_number(self):
+        """test_middle
+        """
+        self.assertAlmostEquals(max_integer([-100]), -100)
+
+    def test_one_number(self):
+        """test_middle
+        """
+        self.assertAlmostEquals(max_integer([100]), 100)
+
+    def test_empty(self):
+        """test_middle
+        """
+        self.assertAlmostEqual(max_integer([]), None)
+
+    def test_not_list(self):
+
+        wi = [1, 2, "mao", 4]
+        self.assertRaises(TypeError)
+
+        wi = [1, 2, [1, 2, 3], 4]
+        self.assertRaises(TypeError)
+
+        wi = [1, 2, (1, 2, 3), 4]
+        self.assertRaises(TypeError)
+
+        wi = (2, 2)
+        self.assertRaises(TypeError)
+
+        wi = "hello"
+        self.assertRaises(TypeError)
+
+    def test_none_and_zero(self):
+
+        wi = []
+        self.assertAlmostEqual(max_integer(wi), None)
+
+        wi = [0, 0, 0, 0]
+        self.assertAlmostEqual(max_integer(wi), 0)
+
+        self.assertAlmostEqual(max_integer(), None)
 
 
 if __name__ == '__main__':
