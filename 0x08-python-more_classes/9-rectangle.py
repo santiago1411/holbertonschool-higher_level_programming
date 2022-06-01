d8b971f9928e5bc9815d69bbfe4a291c02f269f5
santiago1411 <98724484+santiago1411@users.noreply.github.com>
2022-06-01 06:19:27 -0500

    Corrección

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -37,7 +37,7 @@ class Rectangle:
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be an integer")
+            raise ValueError("width must be >= 0 ")
         self.__width = value
 
     @height.setter
@@ -47,7 +47,7 @@ class Rectangle:
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be an integer")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
