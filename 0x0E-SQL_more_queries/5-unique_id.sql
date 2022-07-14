a286fa37de824d2d7b9886f68bc5f5a4164fbf18
santiago1411 <4547@holbertonschool.com>
2022-07-14 14:05:23 -0500

    more sql

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- that creates the table 
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,3 @@
+-- that creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(256) NOT NULL, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id));
