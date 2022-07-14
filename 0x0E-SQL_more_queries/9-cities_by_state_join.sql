fa98001313912871c8c8b9e1e9a69212bcbabccd
santiago1411 <4547@holbertonschool.com>
2022-07-14 14:28:08 -0500

    more sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- that lists all cities contained in the database 
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.states.id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.states_id = states.id ORDER BY cities.id ASC;
