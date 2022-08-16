abe86fcf735ef8079732acfeff003318aa197f43
santiago1411 <4547@holbertonschool.com>
2022-08-16 11:00:33 -0500

    punto 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-const args = process.argv;
-function compareNumbers (a, b) {
-  return b - a;
-}
-if (args.length <= 3) {
-  console.log(0);
-} else {
-  const numbers = args.slice(2).map((number) => {
-    return parseInt(number);
-  });
-  numbers.sort(compareNumbers);
-  console.log(numbers[1]);
-}
+array = array.map(Number);
+array = [... new Set(array)];
+array = array.sort((a, b) => {return b - a});
+console.log(array[1]);
