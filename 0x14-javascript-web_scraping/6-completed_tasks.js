9e9cc1a1bfb42f6b8547ba5a991f43ec8ec2c03e
santiago1411 <4547@holbertonschool.com>
2022-08-17 14:06:31 -0500

    appis

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const axios = require('axios').default;
+const dict = {};
+
+axios.get(args[2])
+  .then(function (response) {
+    for (const elem of response.data) {
+      if (elem.completed === true) {
+        if (dict[elem.userId] === undefined) {
+          dict[elem.userId] = 1;
+        } else {
+          dict[elem.userId] += 1;
+        }
+      }
+    }
+    console.log(dict);
+  });
