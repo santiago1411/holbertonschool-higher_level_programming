4880bbf4edb8179c2610634ea1cc2747d17dafda
santiago1411 <4547@holbertonschool.com>
2022-08-17 12:57:27 -0500

    appis

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+const arg = process.argv[2];
+const axios = require('axios').default;
+
+axios.get(arg)
+  .then(function (response) {
+    // Success execution
+    let count = 0;
+    for (const key of response.data.results) {
+      for (const elem of key.characters) {
+        if (elem.endsWith('18/')) {
+          count++;
+          break;
+        }
+      }
+    }
+    console.log(count);
+  });
