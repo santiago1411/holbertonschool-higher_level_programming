9fd5437e6dfa98949762fc3780d6363a87e86342
santiago1411 <4547@holbertonschool.com>
2022-09-02 21:51:53 -0500

    punto 10

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+Python script
 """
 
 
@@ -8,6 +9,9 @@ from sys import argv
 
 if __name__ == "__main__":
     """
+    Script that takes in a URL, sends a request to the URL
+    and displays the value of the variable X-Request-Id in
+    the response header
     """
     html = requests.get(argv[1])
     print((html.headers).get("X-Request-Id"))
