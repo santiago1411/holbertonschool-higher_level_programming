919ef06ed47998602df6f2ad2a7b3cef21fae2a8
santiago1411 <4547@holbertonschool.com>
2022-09-02 21:52:33 -0500

    punto 10

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+Python script
 """
 
 
@@ -8,6 +9,9 @@ from sys import argv
 
 if __name__ == "__main__":
     """
+    Script that takes in a URL and an email address, sends a
+    POST request to the passed URL with the email as a parameter,
+    and finally displays the body of the response.
     """
     values = {"email": argv[2]}
     r = requests.post(argv[1], values)
